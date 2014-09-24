4a936e8a6d39df3e0bca970497b74e143fe23b09
Brendan Collins <bcollins@blueraster.com>
2014-09-23 23:05:48 -0400

    tweaking setup.py

--- setup.py
+++ setup.py
@@ -50,11 +50,11 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='sample setuptools development',
+    keywords='esri agol pandas',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=find_packages(exclude=['agolpandas*', 'tests*']),
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
